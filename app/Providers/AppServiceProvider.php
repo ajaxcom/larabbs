a6aa1bffd75f9129c341b848593b8a55a792531a
vagrant <vagrant@homestead>
2018-10-26 09:00:33 +0000

    Add manage contents permission ad visit horion dashboard permission

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,13 +21,10 @@ class AppServiceProvider extends ServiceProvider
 
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
-        //
+        if (app()->isLocal()) {
+            $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
+        }
     }
 }
