02287f5c50f92faec149aa4b2cb1b67d3b6a4d9a
vagrant <vagrant@homestead>
2018-11-01 07:17:20 +0000

    User default avatar after signup

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -9,6 +9,13 @@ use App\Models\User;
 
 class UserObserver
 {
+    public function saveing(User $user)
+    {
+        if (empty($user->avatar)){
+            $user->avatar = "https://s1.ax1x.com/2018/11/01/ifanNq.jpg";
+        }
+    }
+
     public function creating(User $user)
     {
         //
